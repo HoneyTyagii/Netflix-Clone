9677004929442845cb999ae4df25e0c869b8a55c
Honey Tyagi <honeyt290@gmail.com>
2023-06-01 00:21:50 +0530

    commit

--- src/components/content-rows.tsx
+++ src/components/content-rows.tsx
@@ -1,10 +1,11 @@
-import { useEffect, useState } from "react";
-import { ENDPOINT } from "../common/endpoints";
+import React, { useEffect, useState } from "react";
 import { MovieResponse, MovieResult, fetchRequest } from "../common/api";
+import { ENDPOINT } from "../common/endpoints";
 type RowProp = {
     endpoint: string;
     title: string;
 };
+const CARD_WIDTH = 200;
 export default function ContentRows({ title, endpoint}: RowProp) {
     const [rowData, setRowData] = useState<MovieResult[]>([]);
     async function fetchRowData() {
@@ -13,24 +14,33 @@ export default function ContentRows({ title, endpoint}: RowProp) {
         );
         setRowData(response.results);
     }
-    function createImageURL(path: string) {
-        return `${import.meta.env.VITE_BASE_IMAGE_URI}/${path}`;
+    function createImageURL(path: string, width: number) {
+        return `${import.meta.env.VITE_BASE_IMAGE_URI}/w${width}/${path}`;
     }
     useEffect(() => {
         fetchRowData();
     }, []);
-    return <section>
+    return (
+    <section>
         <h2 className="mb-4">{title}</h2>
-        <section className="flex flex-nowrap overflow-x-auto">
+        <section className="flex flex-nowrap gap-2">
             {rowData?.map((row) => {
-                const { id, title, poster_path } = row;
+                const { id, title, poster_path, backdrop_path } = row;
                 console.log(row); 
+
+
                 return (
-                     <section key={id} className="h-[200px] w-[200px] flex-none">
-                        <img className="h-full w-full object-contain" src={createImageURL(poster_path)} alt={title} />
+                     <section key={id} 
+                     className="aspect-square flex-none overflow-hidden rounded-md">
+                        <img 
+                        loading="lazy"
+                        className="h-full w-full" 
+                        src={createImageURL(poster_path, CARD_WIDTH)} 
+                        alt={title} />
                      </section>
                 );
             })}
         </section>
     </section>
+    );
 }
